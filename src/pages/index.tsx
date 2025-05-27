68b0dd02451dc3b5343036a5b19a3113b49cf8a9
root <root@DESKTOP-HsnMkt>
2025-05-27 20:13:29 +0700

    gnti format

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import Sidebar from "@/components/sidebar";
 import Header from "@/components/header";
 import Home from "@/pages/Home";
+import Document from "./DocumentPage";
+import Setting from "./SettingPage";
+import Statistik from "./SettingPage";
 
 export default function IndexPage() {
   return (
@@ -10,6 +13,9 @@ export default function IndexPage() {
         <Header />
         <div className="p-6 bg-gray-50 flex-1 overflow-y-auto">
           <Home />
+          <Document/>
+          <Setting/>
+          <Statistik/>
         </div>
       </div>
     </div>
